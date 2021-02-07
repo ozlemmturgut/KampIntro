cef890f43c19e6e47905d64d254638869922208e
ozlemmturgut <55889487+ozlemmturgut@users.noreply.github.com>
2021-02-07 21:16:58 +0300

    7.01.2021

--- KampIntro/Program.cs
+++ KampIntro/Program.cs
@@ -6,7 +6,44 @@ namespace KampIntro
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //type safety = tip güvenliği
+            //Do not repeat yourself = Kendini tekrarlama
+            //deger tutucu, alias
+
+            string kategoriEtiketi = "Kategoriler";
+            int ogrenciSayisi = 32000;
+            double faizOrani = 1.45;
+            bool sistemeGGirisYapmisMi = false; //true
+            double dolarDun = 7.35;
+            double dolarBugun = 7.45;
+
+
+            if (dolarDun > dolarBugun)
+            {
+                Console.WriteLine("Azalış butonu");
+            }
+            else if (dolarDun < dolarBugun)
+            {
+                Console.WriteLine("Artış butonu");
+            }
+            else
+            {
+                Console.WriteLine("Eşittir butonu");
+            }
+
+
+            if (sistemeGGirisYapmisMi == true)
+            {
+                Console.WriteLine("Kulanıcı ayarları butonu");
+            }
+            else
+            {
+                Console.WriteLine("Giriş yap butonu");
+            }
+
+            Console.WriteLine(kategoriEtiketi);
+
+
         }
     }
 }
