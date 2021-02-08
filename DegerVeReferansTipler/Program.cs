a0876ba1b192d1c948a08ed0b03b761129649c39
ozlemmturgut <55889487+ozlemmturgut@users.noreply.github.com>
2021-02-09 01:45:23 +0300

    09.02.2021

---
+++ DegerVeReferansTipler/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace DegerVeReferansTipler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sayi1 = 10;
+            int sayi2 = 30;
+            sayi1 = sayi2;
+            sayi2 = 62;
+            //sayi1 ? 30
+
+            int[] sayilar1 = new int[] { 10, 20, 30 };
+            int[] sayilar2 = new int[] { 100, 200, 300 };
+            sayilar1 = sayilar2;
+            sayilar2[0] = 999;
+            //sayilar1[0] ? 999
+
+            // int, decimal, float, double, bool == değer tip
+            // değer tip --> stack'te gerçekleşir
+            // array, class, interface == referans tip
+            //değişken stack'te tanımlanır. new deyince
+            //heap'te yeni alan oluşturulur ve
+            //değerler heap'e yazdırılır.
+        }
+    }
+}
---
+++ Donguler/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Donguler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //array = dizi
+            string[] kurslar = new string[]
+            {
+                "Yazılım Geliştirici Yetiştirme Kampı", 
+                "Programlamaya başlangıç için temel kurs" , 
+                "Java", "Python", "C#" };
+
+            // i=i+2     ==>     i+=2 
+            for (int i = 0; i < kurslar.Length; i++)
+            {
+                Console.WriteLine(kurslar[i]);
+            }
+            Console.WriteLine("");
+
+            Console.WriteLine("For bitti");
+
+            Console.WriteLine("");
+
+            foreach (string kurs in kurslar)
+            {
+                Console.WriteLine(kurs);
+            }
+
+            Console.WriteLine("");
+
+            Console.WriteLine("Sayfa sonu - footer");
+        }
+    }
+}
