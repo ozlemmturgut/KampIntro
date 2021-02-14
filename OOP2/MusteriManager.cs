9636a5d0c82209104bc73b438a6349c8c8a8fd63
ozlemmturgut <55889487+ozlemmturgut@users.noreply.github.com>
2021-02-14 22:05:41 +0300

    14.02.20021

--- OOP2/MusteriManager.cs
+++ OOP2/MusteriManager.cs
@@ -7,6 +7,7 @@ namespace OOP2
     //İş sınıfları --> Business classes
     class MusteriManager
     {
+
         public void Ekle(Musteri musteri)
         {
             //if (musteri.)
---
+++ OOP3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OOP3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IKrediManager ihtiyacKrediManager = new IhtıyacKrediManager();
+
+            IKrediManager tasitKrediManager = new TasitKrediManager();
+
+            IKrediManager konutKrediManager = new KonutKrediManager();
+
+
+            BasvuruManager basvuruManager = new BasvuruManager();
+            //basvuruManager.BasvuruYap(tasitKrediManager);
+
+            List<IKrediManager> krediler = new List<IKrediManager>() { tasitKrediManager, ihtiyacKrediManager };
+
+            basvuruManager.KrediOnBilgilendirmesiYap(krediler);
+        }
+    }
+}
